ce8992543f8201bbde08a18603ac804f2ed980c3
Mardiatun <153040657+Mardiatun@users.noreply.github.com>
2023-12-06 10:57:04 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,108 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'CHICKEN RAMEN',
+        image: 'chicken ramen.jpg',
+        price: 50000
+    },
+    {
+        id: 2,
+        name: 'TONKOTSU SPICY',
+        image: 'ramen tonkotsu spicy.jpg',
+        price: 55000
+    },
+    {
+        id: 3,
+        name: 'YUZU SHIO',
+        image: 'yuzu shio.jpg',
+        price: 60000
+    },
+    {
+        id: 4,
+        name: 'SPICY CHICKEN RAMEN',
+        image: 'spicy chicken ramen.jpg',
+        price: 65000
+    },
+    {
+        id: 5,
+        name: 'TONKOTSU RAMEN',
+        image: 'tonkotsu-black.jpg',
+        price: 70000
+    },
+    {
+        id: 6,
+        name: 'WONTON CHICKEN RAMEN',
+        image: 'wonton chicken ramen.jpg',
+        price: 75000
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Cart</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
